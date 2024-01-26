8c483a7e6c6d1a966f59e98156050eced7d1b4cf
GioWashel <giowashel@gmail.com>
2024-01-25 19:04:30 -0500

    created add controller

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
 const port = 3000; 
 
-
+app.use(cors());
 app.get('/', (req, res) => {
     res.send('Arithmetic service - Hello World!');
 });
 
+app.get('/add', (req, res) => {
+   
+    const num1 = req.query.num1; ;
+    const num2 = req.query.num2;
+    if (isNaN(num1) || isNaN(num2)) {
+        return res.status(400).json({ error: 'Invalid input. Please provide valid numbers.' });
+    }
+    const sum = parseFloat(num1) + parseFloat(num2);
+    res.json({ result: sum});
+});
 app.listen(port);
