834873e70c15998f2c2b2b801fe1c32305ad76dd
gwashel <gwashel@iu.edu>
2024-02-08 19:43:36 -0500

    added controllers

--- src/index.js
+++ src/index.js
@@ -11,4 +11,13 @@ app.get('/', (req, res) => {
 app.get('/add/:n/:m', (req, res) => {
     res.json(Number(req.params.n) + Number(req.params.m));
 });
+app.get('/sub/:n/:m', (req, res ) => {
+    res.json(Number(req.params.n) - Number(req.params.m));
+});
+app.get('/mul/:n/:m', (req, res ) => {
+    res.json(Number(req.params.n) * Number(req.params.m));
+});
+app.get('/div/:n/:m', (req, res ) => {
+    res.json(Number(req.params.n) / Number(req.params.m));
+});
 app.listen(port);
